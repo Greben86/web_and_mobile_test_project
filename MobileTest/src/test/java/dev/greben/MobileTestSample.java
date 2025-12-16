99024a71f0e66eadf2fac3e30000b6480bdb6763
Viktor <grebenvictor@yandex.ru>
2025-12-16 20:40:07 +0300

    Добавлены мобильные и веб тесты

--- MobileTest/src/test/java/dev/greben/MobileTestSample.java
+++ MobileTest/src/test/java/dev/greben/MobileTestSample.java
@@ -1,10 +1,10 @@
 package dev.greben;
 
-import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class MobileTestSample {
@@ -14,40 +14,37 @@ public class MobileTestSample {
     @BeforeMethod
     public void setUp() throws Exception {
         helper = new DriverHelper("http://127.0.0.1:4723");
+        Thread.sleep(100);
     }
 
-    @Test
-    public void searchForExistingPage() throws InterruptedException {
+    @DataProvider(name = "queries")
+    public Object[][] createDataQuery() {
+        return new Object[][] {
+                { "Москва" },
+                { "Сочи" },
+                { "Казань" },
+        };
+    }
+
+    @Test(dataProvider = "queries")
+    public void searchForExistingPage(String query) throws InterruptedException {
         helper.findElementById("fragment_onboarding_skip_button").click();
         Thread.sleep(100);
 
         // Элементы управления поиском
         helper.findElementById("search_container").click();
         Thread.sleep(100);
-        helper.findElementById("search_src_text").sendKeys("Москва");
+        helper.findElementById("search_src_text").sendKeys(query);
         // Ожидаем отображение результатов поиска
         Thread.sleep(2000); // Дожидаемся появления результата
-
-        // Проверяем наличие найденной страницы
-        final var resultTitle = helper.findElementByText("Redirected from Москва");
-        assertNotNull(resultTitle.getText());
-    }
-
-    @Test
-    public void scrollPage() throws InterruptedException {
-        helper.findElementById("fragment_onboarding_skip_button").click();
-        Thread.sleep(100);
-
-        // Элементы управления поиском
-        helper.findElementById("search_container").click();
-        Thread.sleep(100);
-        helper.findElementById("search_src_text").sendKeys("Москва");
-        // Ожидаем отображение результатов поиска
+        helper.findElementByText("Redirected from " + query).click();
         Thread.sleep(2000); // Дожидаемся появления результата
+        helper.findElementById("closeButton").click();
+        Thread.sleep(100);
+        helper.findElementById("page_contents_container").getText();
 
         // Проверяем наличие найденной страницы
-        final var resultTitle = helper.findElementByText("Redirected from Москва");
-        assertNotNull(resultTitle.getText());
+        assertTrue(helper.findElementById("page_contents_container").isDisplayed());
     }
 
     @Test
